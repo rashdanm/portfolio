2a5c3318b82d71e0348d50df87b99b68190d7d11
RashBM <57621825+RashBM@users.noreply.github.com>
2022-01-10 13:01:01 +0000

    update cv and edit contact section

--- components/Contact.js
+++ components/Contact.js
@@ -11,7 +11,7 @@ const Contact = () => {
           Stay <span className="text-green-700">Connected</span>.
         </div>
         <p className="max-w-screen-sm text-lg">
-          If {`there's`} anything {`you'd`} like to ask me or if {`you'd`}
+          If {`there's`} anything {`you'd`} like to ask me, or if {`you'd`} just
           like to say hello, then {`I'd`} love to hear from you. Simply fill out
           the contact form or drop me an email.
         </p>
